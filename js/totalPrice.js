2663d1e9f996b8fe0a2f2f83b558e5fa00d8caad
tonybnya <nya.tony2010@gmail.com>
2025-01-31 18:59:31 +0100

    feat: JS function to calculate the average salary of a list of employees

--- js/totalPrice.js
+++ js/totalPrice.js
@@ -5,20 +5,20 @@
  * representing products and returns the total price of all products.
  * Each object has keys name, price, and quantity.
  *
-*/
+ */
 const products = [
-    { name: "Laptop", price: 1000, quantity: 2 },
-    { name: "Phone", price: 500, quantity: 3 },
-    { name: "Tablet", price: 300, quantity: 1 }
+  { name: "Laptop", price: 1000, quantity: 2 },
+  { name: "Phone", price: 500, quantity: 3 },
+  { name: "Tablet", price: 300, quantity: 1 },
 ];
 
 const totalPrice = (products) => {
-    // Your code here
-    let totalPrice = 0;
-    for (let i = 0; i < products.length; ++i) {
-        totalPrice += products[i].price;
-    }
-    return totalPrice;
-}
+  // Your code here
+  let total = 0;
+  products.forEach((product) => {
+    total += product.price * product.quantity;
+  });
+  return total;
+};
 
 console.log(totalPrice(products));
