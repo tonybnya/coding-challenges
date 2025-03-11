f1947aba9f15d96a9f80e2caf5ce5434630d4683
tonybnya <nya.tony2010@gmail.com>
2025-03-11 10:35:06 +0100

    feat: add Pair Sum solution

---
+++ dsa/algo_002_pair_sum.py
@@ -0,0 +1,40 @@
+"""
+Given an array of integers sorted in ascending order and a target value,
+return the indexes of any pair of numbers in the array that sum to the target.
+The order of the indexes in the result doesn't matter.
+If no pair is fund, return an empty array.
+"""
+
+
+# Naive Solution
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+def pair_sum_1(nums: list[int], target: int) -> list[int]:
+    n: int = len(nums)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+    return []
+
+
+# Optimal Solution
+# Time Complexity: O()
+# Space Complexity: O()
+def pair_sum_2(nums: list[int], target: int) -> list[int]:
+    n: int = len(nums)
+    l, r = 0, n - 1
+
+    while l < r:
+        s: int = nums[l] + nums[r]
+
+        if s > target:
+            r -= 1
+        if s < target:
+            l += 1
+        else:
+            return [l, r]
+
+    return []
