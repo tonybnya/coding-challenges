f39b20587e56de56a4546723900812fbcc5206d2
tonybnya <nya.tony2010@gmail.com>
2025-03-11 11:18:02 +0100

    docs: add some test cases and add comments to the test file for Palindrome algorithm

--- dsa/tests/test_001_palindrome.py
+++ dsa/tests/test_001_palindrome.py
@@ -1,19 +1,26 @@
+"""
+Test file for Palindrome algorithm.
+"""
+
 import pytest
 
 from algo_001_palindrome import is_palindrome
 
 
 def load_test_cases(filename):
+    """
+    Function to load test cases from a text file.
+    """
     test_cases = []
+
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             line = line.strip()
-            if line:  # Ignore empty lines
-                test_input, expected = line.rsplit(
-                    ",", 1
-                )  # Split input and expected value
-                expected = expected.strip().lower() == "true"  # Convert to boolean
+            if line:
+                test_input, expected = line.rsplit(",", 1)
+                expected = expected.strip().lower() == "true"
                 test_cases.append((test_input, expected))
+
     return test_cases
 
 
@@ -21,4 +28,7 @@ def load_test_cases(filename):
     "test_input, expected", load_test_cases("tests/test_001_palindrome.txt")
 )
 def test_is_palindrome(test_input, expected):
+    """
+    Test for Optimal Solution.
+    """
     assert is_palindrome(test_input) == expected
