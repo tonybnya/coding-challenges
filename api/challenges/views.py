3a958f3ac71228b3421c0431c9ee47fd209f3f95
tonybnya <nya.tony2010@gmail.com>
2025-04-15 21:01:34 +0100

    update

--- api/challenges/views.py
+++ api/challenges/views.py
@@ -29,14 +29,14 @@ class ChallengeViewSet(viewsets.ModelViewSet):
             )
 
 
-# def create_superuser_api(request):
-#     User = get_user_model()
-#     username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-#     email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-#     password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
-#
-#     if not User.objects.filter(username=username).exists():
-#         User.objects.create_superuser(username=username, email=email, password=password)
-#         return JsonResponse({"message": "Superuser created successfully"}, status=201)
-#     else:
-#         return JsonResponse({"message": "Superuser already exists"}, status=200)
+def create_superuser_api(request):
+    User = get_user_model()
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        return JsonResponse({"message": "Superuser created successfully"}, status=201)
+    else:
+        return JsonResponse({"message": "Superuser already exists"}, status=200)
