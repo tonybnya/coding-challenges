7fbee6ed3b7cf2adb484d45acba828295c01ec04
tonybnya <nya.tony2010@gmail.com>
2025-04-15 20:59:08 +0100

    temporary suspend url to create a superuser

--- api/challenges/views.py
+++ api/challenges/views.py
@@ -3,9 +3,9 @@ import os
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from .models import Challenge
 from .serializers import ChallengeSerializer
@@ -29,14 +29,14 @@ class ChallengeViewSet(viewsets.ModelViewSet):
             )
 
 
-def create_superuser_api(request):
-    User = get_user_model()
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        return JsonResponse({"message": "Superuser created successfully"}, status=201)
-    else:
-        return JsonResponse({"message": "Superuser already exists"}, status=200)
+# def create_superuser_api(request):
+#     User = get_user_model()
+#     username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+#     email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+#     password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
+#
+#     if not User.objects.filter(username=username).exists():
+#         User.objects.create_superuser(username=username, email=email, password=password)
+#         return JsonResponse({"message": "Superuser created successfully"}, status=201)
+#     else:
+#         return JsonResponse({"message": "Superuser already exists"}, status=200)
