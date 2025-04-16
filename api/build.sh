a42ea9cb34f9491da4e7110e07cf278dabac796e
tonybnya <nya.tony2010@gmail.com>
2025-04-16 13:47:12 +0100

    update build.sh

--- api/build.sh
+++ api/build.sh
@@ -11,9 +11,9 @@ python3 manage.py collectstatic --no-input
 # # Apply model changes - this is safe and won't reset data
 # echo "Running makemigrations..."
 # python manage.py makemigrations challenges --no-input
-#
-# # Run migrations - this updates schema but preserves data
-# echo "Running migrate..."
-# python3 manage.py migrate --no-input
+
+# Run migrations - this updates schema but preserves data
+echo "Running migrate..."
+python3 manage.py migrate --no-input
 
 echo "Build script completed successfully."
