92e84d7402b1c34e5b491e6b4bc5f50943b727f1
tonybnya <nya.tony2010@gmail.com>
2025-04-18 21:23:08 +0100

    add book icon for Book Search App

--- api/core/settings.py
+++ api/core/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
