72f8a9808d21b18f5ab72ae2003232625797ea03
tonybnya <nya.tony2010@gmail.com>
2025-04-24 12:40:27 +0100

    fix: add localhost option for the API_URL

--- full-stack/src/pages/Home.jsx
+++ full-stack/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import CardChallenge from "../components/CardChallenge";
 import Spinner from "../components/Spinner";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || http://localhost:8000/api;
 
 console.log("VITE_API_URL:", API_URL);
 
