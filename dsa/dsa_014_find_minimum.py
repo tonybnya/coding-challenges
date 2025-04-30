6140d9989b4087cc9bbee7f3b246732c41fc69f5
tonybnya <nya.tony2010@gmail.com>
2025-04-30 09:08:41 +0100

    feat: add Find Minimum problem

---
+++ dsa/dsa_014_find_minimum.py
@@ -0,0 +1,24 @@
+"""
+014. Find minimum.
+Implement the "find minimum" algorithm in Python by completing
+the `find_minimum()` function. It accepts a list of integers `nums`
+and returns the smallest number in the list.
+"""
+
+from typing import Optional
+
+
+def find_minimum(nums: list[int]) -> Optional[int]:
+    """
+    Solution.
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+    if not nums:
+        return None
+
+    minimum = float("inf")
+    for num in nums:
+        if num < minimum:
+            minimum = num
+    return minimum
